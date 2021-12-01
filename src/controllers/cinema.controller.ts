6fce51203c3114b88b3468a17048a440dcc40681
Ender <endernull@icloud.com>
2021-12-01 06:17:44 +0100

    error statusCodes

--- src/controllers/cinema.controller.ts
+++ src/controllers/cinema.controller.ts
@@ -1,24 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { MessagePattern, Payload, Transport } from '@nestjs/microservices';
 import { IdPayload } from 'src/models/common.interface';
 import { CinemaService } from 'src/services/cinema.service';
 
 @Controller()
 export class CinemaController {
+  private readonly logger = new Logger('CinemaController');
+
   constructor(private readonly cinemaService: CinemaService) {}
 
   @MessagePattern('cinemas', Transport.TCP)
   async cinemas() {
-    return this.cinemaService.getCinemas();
+    return this.cinemaService.getCinemas().catch((ex) => {
+      this.logger.error(ex.message);
+      return ex.response;
+    });
   }
 
   @MessagePattern('cinema', Transport.TCP)
   async cinema(@Payload() data: IdPayload) {
-    return this.cinemaService.getCinema(data.id);
+    return this.cinemaService.getCinema(data.id).catch((ex) => {
+      this.logger.error(ex.message);
+      return ex.response;
+    });
   }
 
   @MessagePattern('cinema/pro', Transport.TCP)
   async cinemaPro(@Payload() data: IdPayload) {
-    return this.cinemaService.getCinemaPro(data.id);
+    return this.cinemaService.getCinemaPro(data.id).catch((ex) => {
+      this.logger.error(ex.message);
+      return ex.response;
+    });
   }
 }
--- src/services/cinema.service.ts
+++ src/services/cinema.service.ts
@@ -1,5 +1,13 @@
 import { HttpService } from '@nestjs/axios';
-import { CACHE_MANAGER, Inject, Injectable, Logger } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  HttpStatus,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { Cache } from 'cache-manager';
 import {
   Cinema,
@@ -64,19 +72,22 @@ export class CinemaService {
         await this.cacheManager.set(`cinema/${id}`, resp, { ttl: 3600 });
         return resp;
       } catch (exception) {
-        this.logger.error(exception.message);
-        return {
-          statusCode: 500,
-          error: 'Internal Server Error',
-          message: exception.message,
-        };
+        throw new InternalServerErrorException(
+          {
+            statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+            message: exception.message,
+          },
+          exception.message,
+        );
       }
     } else {
-      return {
-        statusCode: 404,
-        error: 'Not Found',
-        message: `Resource with ID '${id}' was not found`,
-      };
+      throw new NotFoundException(
+        {
+          statusCode: HttpStatus.NOT_FOUND,
+          message: `Resource with ID '${id}' was not found`,
+        },
+        `Resource with ID '${id}' was not found`,
+      );
     }
   }
 
@@ -113,137 +124,130 @@ export class CinemaService {
         await this.cacheManager.set(`cinema/${id}/pro`, resp, { ttl: 3600 });
         return resp;
       } catch (exception) {
-        this.logger.error(exception.message);
-        return {
-          statusCode: 500,
-          error: 'Internal Server Error',
-          message: exception.message,
-        };
+        throw new InternalServerErrorException(
+          {
+            statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+            message: exception.message,
+          },
+          exception.message,
+        );
       }
     } else {
-      return {
-        statusCode: 404,
-        error: 'Not Found',
-        message: `Resource with ID '${id}' was not found`,
-      };
+      throw new NotFoundException(
+        {
+          statusCode: HttpStatus.NOT_FOUND,
+          message: `Resource with ID '${id}' was not found`,
+        },
+        `Resource with ID '${id}' was not found`,
+      );
     }
   }
 
   async getMoviesPalafox(id: string): Promise<Movie[]> {
-    try {
-      const response = await lastValueFrom(
-        this.httpService.get(cinemas[id].source),
-      );
-      const $ = cheerio.load(response.data);
-      return (await Promise.all(
-        $('.views-field-nothing')
-          .map(async (_, value) => {
-            const source = `https://www.cinespalafox.com${$(value)
-              .find('a')
-              .attr('href')}`;
-            const filmResponse = await lastValueFrom(
-              this.httpService.get(source),
-            );
-            const $2 = cheerio.load(filmResponse.data);
-            const id = /cartelera\/([\w-]+)/.exec(source)[1];
-            const name = $2('h1').text();
-            const poster = $2('.imagecache-cartelDetalle').attr('src');
-            const trailer = $2('#urlvideo').text();
-            const synopsis = $2('.sinopsis p').first().text();
-            const details = $2('.datos span');
-            const genres = details.eq(0).text().split(', ');
-            const duration = parseInt(
-              details
-                .eq(details.length - 3)
-                .text()
-                .replace(/[^\d]/g, ''),
-            );
-            const durationReadable = minutesToString(duration);
-            const director = details.eq(details.length - 2).text();
-            const actors = details
-              .eq(details.length - 1)
+    const response = await lastValueFrom(
+      this.httpService.get(cinemas[id].source),
+    );
+    const $ = cheerio.load(response.data);
+    return (await Promise.all(
+      $('.views-field-nothing')
+        .map(async (_, value) => {
+          const source = `https://www.cinespalafox.com${$(value)
+            .find('a')
+            .attr('href')}`;
+          const filmResponse = await lastValueFrom(
+            this.httpService.get(source),
+          );
+          const $2 = cheerio.load(filmResponse.data);
+          const id = /cartelera\/([\w-]+)/.exec(source)[1];
+          const name = $2('h1').text();
+          const poster = $2('.imagecache-cartelDetalle').attr('src');
+          const trailer = $2('#urlvideo').text();
+          const synopsis = $2('.sinopsis p').first().text();
+          const details = $2('.datos span');
+          const genres = details.eq(0).text().split(', ');
+          const duration = parseInt(
+            details
+              .eq(details.length - 3)
               .text()
-              .split(', ');
-            const sessions = [];
-            const parsedDate = $2('.horarios ul li').eq(0).text();
-            const splitDate = parsedDate.split('/');
-            const date = `${splitDate[2]}-${splitDate[1]}-${splitDate[0]}`;
-            const schedules = $2('.horarios ul li').eq(1).find('a');
-            schedules.each((index) => {
-              const inputMatch = /Sala (\d+) - ([\d:]+)/.exec(
-                schedules.eq(index).text(),
-              );
-              const session: Session = {
-                date,
-                time: inputMatch[2],
-                room: inputMatch[1],
-                url: schedules.eq(index).attr('href'),
-              };
-              sessions.push(session);
-            });
-            const movie: Movie = {
-              id,
-              name,
-              synopsis,
-              duration,
-              durationReadable,
-              sessions,
-              director,
-              actors,
-              genres,
-              poster,
-              trailer,
-              source,
+              .replace(/[^\d]/g, ''),
+          );
+          const durationReadable = minutesToString(duration);
+          const director = details.eq(details.length - 2).text();
+          const actors = details
+            .eq(details.length - 1)
+            .text()
+            .split(', ');
+          const sessions = [];
+          const parsedDate = $2('.horarios ul li').eq(0).text();
+          const splitDate = parsedDate.split('/');
+          const date = `${splitDate[2]}-${splitDate[1]}-${splitDate[0]}`;
+          const schedules = $2('.horarios ul li').eq(1).find('a');
+          schedules.each((index) => {
+            const inputMatch = /Sala (\d+) - ([\d:]+)/.exec(
+              schedules.eq(index).text(),
+            );
+            const session: Session = {
+              date,
+              time: inputMatch[2],
+              room: inputMatch[1],
+              url: schedules.eq(index).attr('href'),
             };
-            return movie;
-          })
-          .toArray(),
-      )) as any;
-    } catch (exception) {
-      this.logger.error(exception.message);
-      return [];
-    }
+            sessions.push(session);
+          });
+          const movie: Movie = {
+            id,
+            name,
+            synopsis,
+            duration,
+            durationReadable,
+            sessions,
+            director,
+            actors,
+            genres,
+            poster,
+            trailer,
+            source,
+          };
+          return movie;
+        })
+        .toArray(),
+    )) as any;
   }
 
   async getMoviesCinesa(id: string): Promise<Movie[]> {
-    try {
-      const response = await lastValueFrom(
-        this.httpService.get(cinemas[id].source),
-      );
-      return response.data.cartelera[0].peliculas.map((item): Movie => {
-        const sessions: Session[] = [];
-        item.cines.forEach((cine) => {
-          cine.tipos.forEach((tipo) => {
-            tipo.salas.forEach((sala) => {
-              sala.sesiones.forEach((sesion) => {
-                sessions.push({
-                  date: response.data.cartelera[0].dia,
-                  time: sesion.hora,
-                  room: sala.sala,
-                  type: sesion.tipo,
-                  url: sesion.ao,
-                });
+    const response = await lastValueFrom(
+      this.httpService.get(cinemas[id].source),
+    );
+    return response.data.cartelera[0].peliculas.map((item): Movie => {
+      const sessions: Session[] = [];
+      item.cines.forEach((cine) => {
+        cine.tipos.forEach((tipo) => {
+          tipo.salas.forEach((sala) => {
+            sala.sesiones.forEach((sesion) => {
+              sessions.push({
+                date: response.data.cartelera[0].dia,
+                time: sesion.hora,
+                room: sala.sala,
+                type: sesion.tipo,
+                url: sesion.ao,
               });
             });
           });
         });
-
-        return {
-          id: item.url,
-          name: item.titulo,
-          duration: item.duracion,
-          durationReadable: minutesToString(item.duracion),
-          sessions,
-          director: item.directores,
-          actors: item.actores.split(', '),
-          genres: item.genero.split(' - '),
-          poster: item.cartel,
-          source: `https://www.cinesa.es/Peliculas/${item.url}`,
-        };
       });
-    } catch (exception) {
-      this.logger.error(exception.message);
-      return [];
-    }
+
+      return {
+        id: item.url,
+        name: item.titulo,
+        duration: item.duracion,
+        durationReadable: minutesToString(item.duracion),
+        sessions,
+        director: item.directores,
+        actors: item.actores.split(', '),
+        genres: item.genero.split(' - '),
+        poster: item.cartel,
+        source: `https://www.cinesa.es/Peliculas/${item.url}`,
+      };
+    });
   }
 }
